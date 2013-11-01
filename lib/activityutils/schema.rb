5930cb8f0f82937cdee591c8430f3142d3ac1994
Gerald Bauer <gerald.bauer@gmail.com>
2013-11-01 11:56:29 +0100

    up schema

--- lib/activityutils/schema.rb
+++ lib/activityutils/schema.rb
@@ -7,12 +7,21 @@ class CreateDb < ActiveRecord::Migration
 create_table :activities do |t|
   t.integer :user_id    # references user
 
-  t.integer :item_id
-  t.string  :item_type    # is polymorphic - check if it works ???
+  t.integer :trackable_id
+  t.string  :trackable_type    # is polymorphic
   
-  t.string  :action    # or use key - why?? why not??
+  t.string  :action    #  holds only create|update|delete - for trackable record
+
+  ## optional free form (free text) activity -- make it into a table of its own? why? why not?
+  #  why - keeps render uniform
+  # why not - creates an extra record/model/table
+  
+  t.text    :text      # optional - text to render
+  t.string  :tmpl      # optional - template key
+
   ### t.string  :params  - add parameters column - why ?? why not??
-  t.timestamps
+  
+  t.timestamps   # - only need created-at - supposed no changes e.g. no modified_at
 end
 
   end # method up
